5a55423ae7e2991df32566c963cea799d9ceebc0
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-16 08:27:11 +0530

    added styling using external stylesheet

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) =>{
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed}/>
