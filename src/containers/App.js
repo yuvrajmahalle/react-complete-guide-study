afe6cfce082dc11e2ed08df8d45ed2ff2a18ac81
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-16 07:54:52 +0530

    understand and added state and get value from state

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,14 +4,23 @@ import './App.css';
 import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
+  state ={
+    persons:[
+      {name:"ashish", age:23},
+      {name:"test", age:32},
+      {name:"xyz", age:24}
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1>Hi, React complete guide</h1>
-        <Person name="Ashish" age="23"/>
-        <Person name="abc" age="24">Hobbies: Racing</Person>
-        <Person name="xyz" age="25"/>
-        <Person name="pqr" age="26" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+         
       </div>
     );
   }
