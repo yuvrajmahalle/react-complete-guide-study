689456c5c2abcf4e2925ab657dd8f4c84352a15f
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-16 08:01:57 +0530

    use setState to manipulate state and event handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,11 +12,21 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () =>{
+    this.setState({
+      persons:[
+        {name:"ashish testing event", age:23},
+        {name:"test", age:32},
+        {name:"xyz", age:24}
+      ]
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, React complete guide</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
