fea52919339e351a608ecfe0308179b0f12ca70c
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-17 17:34:38 +0530

    added useeffect and react.dom for optimization of function components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Radium from 'radium';
 
 const cockpit = (props) => {
+    useEffect(()=>{
+        console.log('[cockpit.js] useEffect');
+        //setTimeout(()=>{
+            //alert('Saved data to cloud!');
+       // },1000);
+        return () => {
+            console.log('cockpit.js cleanup work in use effect');
+        }
+    },[]); // useEffect method which run whenenver compontent rerender.
+
+    useEffect(()=>{
+        console.log('cockpit.js 2nd useEffect');
+        return () => {
+            console.log('cockpit cleanup work in useeffect');
+        }
+    });
+
     return(
         <div>
             <h1>{props.title}</h1>
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,7 +4,12 @@ import Person from './Person/Person';
 class Persons extends Component {
     shouldComponentUpdate(nextProps,nextState){
         console.log('[Persons.js] shouldComponentUpdate');
-        return true;
+        if(nextProps.persons !== this.props.persons){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 
     getSnapshotBeforeUpdate(prevProps,nextState){
