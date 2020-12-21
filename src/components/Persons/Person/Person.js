f18c313b3f15ab3bd72a436749bd17a299a86e5f
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-21 22:56:55 +0530

    add the createref method in person.js to add focus property using ref property

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,6 +4,15 @@ import './Person.css'
 import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component{
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef(); // used to set reference.
+    }
+
+    componentDidMount(){
+        this.inputElementRef.current.focus();  // assigning focus() tot he current inputElementRef which is provided by the creatRef() method inside the constructor.   
+    }
+
     render(){
         console.log('[person.js] rednering..');
         return (
@@ -11,7 +20,10 @@ class Person extends Component{
             <div className="Person">
                 <p onClick={this.props.click}>I'm a {this.props.name} and {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input type="text" value={this.props.name} onChange={this.props.changed}/>
+                <input type="text"
+                    ref={this.inputElementRef}
+                 value={this.props.name} 
+                 onChange={this.props.changed}/>
             </div>
             </Fragment>
         )
