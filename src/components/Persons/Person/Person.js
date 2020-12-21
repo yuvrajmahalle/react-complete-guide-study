fed859a7b7118f72315fef16bea2d79605a7cdb8
yuvrajmahalle <aashishmahalle90@gmail.com>
2020-12-21 22:39:51 +0530

    added prop-types package to get proper warning message for incorrect datatype passed for props in both function and class component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component,Fragment } from 'react';
+import PropTypes from 'prop-types';
 import './Person.css'
 import Aux from '../../../hoc/Auxiliary';
 
@@ -16,6 +17,13 @@ class Person extends Component{
         )
     }
 };
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name : PropTypes.string,
+    age : PropTypes.number,
+    changed:PropTypes.func
+}
      
 
 export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
  
 import Radium from 'radium';
 import classes from './App.css';
